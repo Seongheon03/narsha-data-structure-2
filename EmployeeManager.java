c39e33f0cf9f0b362fa0220effb52dbbd283053d
SeongHeon <dev.seongheon@gmail.com>
2019-11-05 20:49:13 +0900

    GUI 구현중

--- EmployeeManager.java
+++ EmployeeManager.java
@@ -1,85 +1,175 @@
+import javax.swing.*;
+import java.awt.*;
 import java.util.LinkedList;
-import java.util.Scanner;
 
-public class EmployeeManager {
-    LinkedList<Employee> list = new LinkedList<Employee>();
-    Scanner sc = new Scanner(System.in);
+public class EmployeeManager extends BassFrame {
 
-    public void uploadEmployee(){
+    JTextField[] jtf = new JTextField[6];
+    JLabel[] jlb = new JLabel[6];
+    String[] labelText = "사원 코드, 사원 이름, 전화번호, 사원 부서 ( 인사팀 / 개발팀 / 디자인팀 ), 사원 주소, 가입 일자".split(", ");
+    int listIndex = 0;
+
+    JButton left = new JButton("◀");
+    JButton right = new JButton("▶");
+
+    public EmployeeManager(int input, LinkedList<Employee> list) {
+
+        switch(input){
+            case 1:
+                addEmployeePanel(list);
+                break;
+            case 2:
+                PrintEmployeeAll(list);
+                break;
+            case 3:
+                PrintEmployeeByCode(list);
+                break;
+            case 4:
+                RemoveEmployee(list);
+                break;
+            case 5:
+                MoveDepartment(list);
+        }
+
+    }
+
+    public void addEmployeePanel(LinkedList<Employee> list) {
+        setLayout(new GridLayout(0, 1, 5, 5));
+
+        for (int i = 0; i < jtf.length; i++) {
+            JPanel jp = new JPanel();
+            jp.add(jlb[i] = new JLabel(labelText[i]));
+            jp.add(jtf[i] = new JTextField(20));
+            add(jp);
+        }
+
+        JButton addBtn = new JButton("추가");
+        JButton endBtn = new JButton("완료");
+        add(addBtn);
+        add(endBtn);
+        addBtn.addActionListener(e -> uploadEmployee(list));
+        endBtn.addActionListener(e -> dispose());
+    }
+
+    //TODO : 함수명 바꾸기
+    private void uploadEmployee(LinkedList<Employee> list) {
         Employee e = new Employee();
-        int department = 0;
-        Boolean check = true;
+        Boolean codeCheck = false;
+        Boolean departmentCheck = false;
 
-        list.addLast(e);
-        System.out.print("사원 코드 : ");
-        e.setCode(sc.nextInt());
-        System.out.print("사원 이름 : ");
-        e.setName(sc.nextLine());
-        System.out.print("전화번호 : ");
-        e.setPhone(sc.nextLine());
-        System.out.print("사원 부서 (1. 인사팀 / 2. 개발팀 / 3. 디자인팀) :");
-        sc.nextInt(department);
-        while(check){
-            switch(department){
-                case 1:
-                    e.setDepartment(Department.HUMAN_RESOURCE);
-                    check = false;
-                    break;
-                case 2:
-                    e.setDepartment(Department.DEVELOPMENT);
-                    check = false;
-                    break;
-                case 3:
-                    e.setDepartment(Department.DESIGN);
-                    check = false;
-                    break;
-                default:
-                    System.out.println("다시 입력해주세요");
-            }
-        }
-        System.out.print("사원 주소 : ");
-        e.setAddress(sc.nextLine());
-        System.out.print("가입 일자 : ");
-        e.setSignUpDate(sc.nextLine());
-        System.out.println("등록이 완료되었습니다.");
-    }
-
-    void printEmployeeAll(){
-        if(list.size() == 0){
-            System.out.println("등록된 사원이 없습니다.");
-            return;
-        }
-        for(int i = 0; i < list.size(); i++){
-            printEmployeeInformation(i);
-            System.out.println();
-        }
-    }
-
-    void printEmployeeByCode(){
-        String input = sc.nextLine();
-
-        for(int i = 0; i < list.size(); i++){
-            if(list.get(i).getCode().equals(input)){
-                printEmployeeInformation(i);
+        for (int i = 0; i < jtf.length; i++) {
+            if (jtf[i].getText().equals("")) {
+                wmsg("공백이 있습니다.");
                 return;
             }
         }
-        System.out.println("사원을 찾을 수 없습니다.");
+
+        departmentCheck = e.setDepartment(jtf[3].getText().toString());
+        if (departmentCheck == false) {
+            wmsg("부서를 다시 입력해주세요.");
+            return;
+        }
+
+        while (!codeCheck) {
+            e.setCode(jtf[0].getText().toString());
+            codeCheck = true;
+            if (list.size() == 0)
+                break;
+            System.out.println(e.getCode());
+            for (int i = 0; i < list.size(); i++) {
+                if (list.get(i).getCode().equals(e.getCode())) {
+                    wmsg("중복된 사원코드입니다. 다시 입력해 주세요.");
+                    codeCheck = false;
+                    return;
+                }
+            }
+        }
+
+        e.setName(jtf[1].getText().toString());
+        e.setPhone(jtf[2].getText().toString());
+        e.setAddress(jtf[4].getText().toString());
+        e.setSignUpDate(jtf[5].getText().toString());
+        list.addLast(e);
     }
 
-    void removeEmployee(){
-        Employee e = new Employee();
-        int code;
-        int i = 0;
+    public void PrintEmployeeAll(LinkedList<Employee> list){
+        setLayout(new BorderLayout());
+        JLabel jlb = new JLabel();
 
-        code = sc.nextInt();
-        list.get(i);
-        while(i <= list.size()){
-            if(list.get(i))
+        add(left, BorderLayout.WEST);
+        add(right, BorderLayout.EAST);
+        add(printEmployeeInformation(list, listIndex));
+
+        left.setEnabled(listIndex != 0);
+        right.setEnabled(listIndex < (list.size() -1));
+
+        left.addActionListener(e -> {
+            leftButtonAction(list);
+        });
+
+        right.addActionListener(e -> {
+            rightButtonAction(list);
+        });
+
+        if(list.size() == 0){
+            add(jlb = new JLabel("등록된 사원이 없습니다."));
+            return;
         }
     }
 
-    void moveDepartment(int code){
+    public void PrintEmployeeByCode(LinkedList<Employee> list){
+        setLayout(new GridLayout(6,0,5,5));
+
+        JLabel jlb = new JLabel("사원 코드");
+        JTextField jtf = new JTextField(20);
+        JButton check = new JButton("검색");
+
+        add(jlb);
+        add(check);
+
+        for(int i = 0; i<list.size(); i++){
+            if(list.get(i).getCode.equals(jtf.getText().toString())){
+                printEmployeeInformation(list, i);
+                break;
+            }
+        }
+    }
+
+    private JPanel printEmployeeInformation(LinkedList<Employee> list, int i){
+        JPanel showPanel = new JPanel(new GridLayout(0, 1, 5, 5));
+        showPanel.removeAll();
+
+        showPanel.add(new JLabel(labelText[0] + " : " + list.get(i).getCode()));
+        showPanel.add(new JLabel(labelText[1] + " : " + list.get(i).getName()));
+        showPanel.add(new JLabel(labelText[2] + " : " + list.get(i).getPhone()));
+        showPanel.add(new JLabel(labelText[3] + " : " + list.get(i).getDepartment()));
+        showPanel.add(new JLabel(labelText[4] + " : " + list.get(i).getAddress()));
+        showPanel.add(new JLabel(labelText[5] + " : " + list.get(i).getSignUpDate()));
+
+        System.out.println(labelText[0] + " : " + list.get(i).getCode());
+        showPanel.repaint();
+        showPanel.revalidate();
+        return showPanel;
+    }
+
+    public void RemoveEmployee(LinkedList<Employee> list){
 
     }
+
+    public void MoveDepartment(LinkedList<Employee> list){
+
+    }
+
+    private void leftButtonAction(LinkedList<Employee> list) {
+        add(printEmployeeInformation(list, --listIndex));
+        left.setEnabled(listIndex != 0);
+        right.setEnabled(listIndex < (list.size() -1));
+    }
+
+    private void rightButtonAction(LinkedList<Employee> list) {
+        add(printEmployeeInformation(list, ++listIndex));
+        left.setEnabled(listIndex != 0);
+        right.setEnabled(listIndex < (list.size() -1));
+    }
+
 }
