28bd6876bccffe5d3e7a55141a2dbe191f21508d
Seongheon <dev.seongheon@gmail.com>
2019-11-07 22:16:05 +0900

    인사 관리 프로그램
    Java Swing을 사용하여 사원 등록, 사원 전체 조회, 사원 조회, 사원 해고, 사원 부서 이동 기능을 만들었습니다.

--- MainPanel.java
+++ MainPanel.java
@@ -1,9 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionListener;
 import java.util.LinkedList;
 
-public class MainPanel extends BassFrame{
+public class MainPanel extends BaseFrame {
 
     static LinkedList<Employee> list = new LinkedList<Employee>();
     String[] buttonTitle = "사원등록, 사원 전체 조회, 사원 조회, 사원 해고, 사원 부서 이동, 프로그램 종료".split(", ");
@@ -17,25 +16,6 @@ public class MainPanel extends BassFrame{
             jbt[i] = new JButton(buttonTitle[i]);
             add(jbt[i]);
         }
-        Employee employee1 = new Employee();
-        Employee employee2 = new Employee();
-
-        employee1.setCode("1234");
-        employee1.setName("김");
-        employee1.setPhone("1234");
-        employee1.setDepartment("인사팀");
-        employee1.setAddress("대구");
-        employee1.setSignUpDate("19.12.12");
-
-        employee2.setCode("1235");
-        employee2.setName("성");
-        employee2.setPhone("1235");
-        employee2.setDepartment("개발팀");
-        employee2.setAddress("동구");
-        employee2.setSignUpDate("19.12.13");
-
-        list.addLast(employee1);
-        list.addLast(employee2);
 
         jbt[0].addActionListener(e->{
             new EmployeeManager(1, list).setVisible(true);
--- PrintEmployeeInformation.java
+++ PrintEmployeeInformation.java
@@ -5,17 +5,17 @@ import java.util.LinkedList;
 public class PrintEmployeeInformation extends BaseFrame {
 
         public JPanel PrintEmployeeInformation(LinkedList<Employee> list, int i) {
-        JPanel showPanel = new JPanel(new GridLayout(0, 1, 5, 5));
-        showPanel.setPreferredSize(new Dimension(500, 280));
+                JPanel showPanel = new JPanel(new GridLayout(0, 1, 5, 5));
+                showPanel.setPreferredSize(new Dimension(500, 280));
 
-        showPanel.add(new JLabel("사원 번호 : " + list.get(i).getCode(), 0));
-        showPanel.add(new JLabel("사원 이름 : " + list.get(i).getName(),0 ));
-        showPanel.add(new JLabel("전화번호 : " + list.get(i).getPhone(), 0));
-        showPanel.add(new JLabel("사원 부서 : " + list.get(i).getDepartment(),0 ));
-        showPanel.add(new JLabel("사원 주소 : " + list.get(i).getAddress(), 0));
-        showPanel.add(new JLabel("가입 일자 : " + list.get(i).getSignUpDate(), 0));
+                showPanel.add(new JLabel("사원 번호 : " + list.get(i).getCode(), 0));
+                showPanel.add(new JLabel("사원 이름 : " + list.get(i).getName(),0 ));
+                showPanel.add(new JLabel("전화번호 : " + list.get(i).getPhone(), 0));
+                showPanel.add(new JLabel("사원 부서 : " + list.get(i).getDepartment(),0 ));
+                showPanel.add(new JLabel("사원 주소 : " + list.get(i).getAddress(), 0));
+                showPanel.add(new JLabel("가입 일자 : " + list.get(i).getSignUpDate(), 0));
 
-        return showPanel;
-    }
-    
+                return showPanel;
+        }
+
 }
